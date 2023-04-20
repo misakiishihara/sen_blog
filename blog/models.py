e07e2fcfa91ff3b5e3c1053a05acb4343dfdd720
{自分の名前} <misaki.ishihara0004@gmail.com>
2023-04-20 12:13:49 +0900

    added tag fields(Many to Many)

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,16 @@ class Category(models.Model):
     name = models.CharField("Category", max_length=30)
     slug = models.SlugField(unique=True)
 
+    def __str__(self) -> str:
+        return self.name    
+
+class Tag(models.Model):
+    name = models.CharField("Tag", max_length=30)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self) -> str:
+        return self.name    
+
 
 class Post(models.Model):
     title = models.CharField("Title", max_length=200)
@@ -12,7 +22,14 @@ class Post(models.Model):
     updated_at = models.DateTimeField("Updated at", auto_now=True)
     is_published = models.BooleanField("IsPublished", default=False)
 
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        null=True, 
+        blank=True,
+    )
+    
+    tags = models.ManyToManyField(Tag, verbose_name=Tag, blank=True)
 
     def __str__(self):
         return self.title
